5c7e801b0759c62e6b021ac3c2d70d33d6656187
Damian <damjack16@gmail.com>
2019-01-24 22:25:25 +0100

    Added media queries on min-width: 768px and 1024px. Also added two images in gallery

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// HAMBURGER MENU
 const burger = document.querySelector(".hamburger-menu")
 const closeBurger = document.querySelector(".fa-times")
 const openBurger = document.querySelector(".fa-bars")
@@ -7,4 +8,25 @@ burger.addEventListener('click', () => {
     openBurger.classList.toggle("show");
     closeBurger.classList.toggle("show");
     nav.classList.toggle("navShow");
+})
+
+// VIEW MORE GALLERY BUTTON
+const btnView = document.querySelector(".gallery .view-btn");
+const imgOne = document.querySelector(".gallery img:nth-of-type(6)");
+const imgTwo = document.querySelector(".gallery img:nth-of-type(7)");
+
+btnView.addEventListener('click', () => {
+    if (btnView.classList.contains("view-btn")) {
+        btnView.textContent = "SHOW LESS";
+        btnView.classList.add("show-btn");
+        btnView.classList.remove("view-btn");
+        imgOne.style.display = "inline";
+        imgTwo.style.display = "inline";
+    } else if (btnView.classList.contains("show-btn")) {
+        btnView.textContent = "VIEW MORE";
+        btnView.classList.add("view-btn");
+        btnView.classList.remove("show-btn")
+        imgOne.style.display = "none"
+        imgTwo.style.display = "none"
+    }
 })
